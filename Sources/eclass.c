ef2d85771b3e5f01cc41e6f236f27afe09378f4d
Antoine Villeret <antoine.villeret@gmail.com>
2019-04-30 22:40:31 +0200

    remove legacy sys_gui call when building against PurrData

--- Sources/eclass.c
+++ Sources/eclass.c
@@ -859,6 +859,7 @@ static void ewidget_init(t_eclass* c)
  */
 static void eclass_properties_dialog(t_eclass* c)
 {
+#ifndef PURR_DATA
     int i, j, lenght;
     char buffer[1000];
     char temp[1000];
@@ -1004,7 +1005,7 @@ static void eclass_properties_dialog(t_eclass* c)
         }
     }
     sys_gui("}\n");
-    
+#endif
 }
 
 
